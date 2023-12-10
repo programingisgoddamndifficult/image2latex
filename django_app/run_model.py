c3c9a77ff056988410cb8106e5a7a18bbcdce333
Stella <90440706+programingisgoddamndifficult@users.noreply.github.com>
2023-12-10 12:12:55 +0800

    添加gif的显示

--- django_app/run_model.py
+++ django_app/run_model.py
@@ -7,11 +7,11 @@ from model.manager import ModelManager
 
 def predict_by_image_filename(image_filename="2.png"):
     IMAGE_PATH = "LaTeX_OCR_PRO-master/data/images/train/"+image_filename
-    try:
-        print("entering predition")
-        return ModelManager.instance().predict_png(IMAGE_PATH)
-    except:
-        return 'something wrong happend'
+    # try:
+    print("entering predition")
+    return ModelManager.instance().predict_png(IMAGE_PATH)
+    # except:
+    #     return 'something wrong happend'
 
 
 def search_form(request):
@@ -29,8 +29,8 @@ def search(request):
         # predict_string, gif_path = a[0], a[1]
         predict_string = predict_by_image_filename(image_url)
         image_url = "http://localhost:8020/" + image_url.split("/")[-1].split("\\")[-1]
-        gif_path = image_url
-        # gif_path = "http://localhost:8030/" + gif_path.split("/")[-1].split("\\")[-1]
+        gif_path = "LaTeX_OCR_PRO-master/results/vis/"+"vis_82_visualization1.gif"
+        gif_path = "http://localhost:8030/" + gif_path.split("/")[-1].split("\\")[-1]
         return render(request,'search_result.html', {
             'image_url': image_url,
             'gif_path': gif_path,
